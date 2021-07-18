0949bde9ac0cce8ee74037df32fa585983ef8c38
panmau <16801528+panmau@users.noreply.github.com>
2021-07-18 21:15:10 +0200

    Enable Realtime

--- database/database.sql
+++ database/database.sql
@@ -84,3 +84,14 @@ INSERT INTO public.game_states (state) VALUES
 ('Connected'),
 ('InProgress'),
 ('Finished');
+-- Enable Realtime
+begin; 
+  -- remove the realtime publication
+  drop publication if exists supabase_realtime; 
+
+  -- re-create the publication but don't enable it for any tables
+  create publication supabase_realtime;
+commit;
+
+-- add a table to the publication
+alter publication supabase_realtime add table moves;
